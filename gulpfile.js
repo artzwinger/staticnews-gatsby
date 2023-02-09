2fe88d7a98c94f547f7a19539633ad2d0d7db2ae
Art Zwinger <artzwinger@gmail.com>
2023-02-09 22:25:39 +0300

    check s3 deployment

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const gulp = require('gulp')
+const awspublish = require('gulp-awspublish')
+const parallelize = require('concurrent-transform')
+
+// https://docs.aws.amazon.com/cli/latest/userguide/cli-environment.html
+
+const config = {
+  // Required
+  params: {
+    Bucket: process.env.AWS_BUCKET_NAME
+  },
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    signatureVersion: 'v3'
+  },
+  // Optional
+  deleteOldVersions: true, // NOT FOR PRODUCTION
+  region: process.env.AWS_DEFAULT_REGION,
+  distDir: 'public',
+  indexRootPath: true,
+  cacheFileName: '.awspublish',
+  concurrentUploads: 10
+}
+
+gulp.task('deploy', function () {
+  // create a new publisher using S3 options
+  // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property
+  console.log(config)
+  const publisher = awspublish.create(config)
+
+  let g = gulp.src('./' + config.distDir + '/**')
+  // publisher will add Content-Length, Content-Type and headers specified above
+  // If not specified it will set x-amz-acl to public-read by default
+  g = g.pipe(
+    parallelize(publisher.publish(config.headers), config.concurrentUploads)
+  )
+
+  // Invalidate CDN
+
+  // Delete removed files
+  if (config.deleteOldVersions) {
+    g = g.pipe(publisher.sync())
+  }
+  // create a cache file to speed up consecutive uploads
+  g = g.pipe(publisher.cache())
+  // print upload updates to console
+  g = g.pipe(awspublish.reporter())
+  return g
+})
