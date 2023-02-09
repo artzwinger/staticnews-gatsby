b56ff43cc36171b3f70b846b5666ab84c2d493ef
Art Zwinger <artzwinger@gmail.com>
2023-02-09 12:10:11 +0300

    add Gatsby with source-plugin stub in plugins directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,21 +2,16 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `Static news`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `Static news`,
+        siteUrl: `https://www.yourdomain.tld`
     },
-    __key: "images"
-  }]
+    plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", `source-plugin`,
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                "icon": "src/images/icon.png"
+            }
+        },
+    ]
 };
