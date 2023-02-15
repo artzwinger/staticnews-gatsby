3e8c3a7b9d6da37d9fd576fc12e73777891c929e
Art Zwinger <artzwinger@gmail.com>
2023-02-15 17:35:07 +0300

    add images to list posts

--- src/templates/blog-post-archive.js
+++ src/templates/blog-post-archive.js
@@ -1,67 +1,80 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import {graphql, Link} from "gatsby"
 import parse from "html-react-parser"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import {GatsbyImage} from "gatsby-plugin-image";
 
 const BlogIndex = ({
-  data,
-  pageContext: { nextPagePath, previousPagePath },
-}) => {
-  const posts = data.allPost.nodes
+                       data,
+                       pageContext: {nextPagePath, previousPagePath},
+                   }) => {
+    const posts = data.allPost.nodes
 
-  if (!posts.length) {
+    if (!posts.length) {
+        return (
+            <Layout isHomePage>
+                <Seo title="All posts"/>
+                <p>
+                    No blog posts found. Add posts to your WordPress site and they'll
+                    appear here!
+                </p>
+            </Layout>
+        )
+    }
+
     return (
-      <Layout isHomePage>
-        <Seo title="All posts" />
-        <p>
-          No blog posts found. Add posts to your WordPress site and they'll
-          appear here!
-        </p>
-      </Layout>
+        <Layout isHomePage>
+            <Seo title="All posts"/>
+
+            <div className="posts">
+                {posts.map(post => {
+                    const title = post.title
+
+                    const featured_image = {
+                        data: post.featured_image?.childImageSharp?.gatsbyImageData,
+                        alt: post.title,
+                    }
+
+                    return (
+                        <div key={post.slug} className="post">
+                            <article
+                                className="post-list-item"
+                                itemScope
+                                itemType="http://schema.org/Article"
+                            >
+                                <header>
+                                    {featured_image?.data && (
+                                        <GatsbyImage
+                                            image={featured_image.data}
+                                            alt={featured_image.alt}
+                                            style={{marginBottom: 50}}
+                                        />
+                                    )}
+                                    <h2>
+                                        <Link to={`/${post.slug}`} itemProp="url">
+                                            <span itemProp="headline">{parse(title)}</span>
+                                        </Link>
+                                    </h2>
+                                    <small>{post.foreign_created_at}</small>
+                                </header>
+                                <section itemProp="description">{parse(post.description)}</section>
+                            </article>
+                        </div>
+                    )
+                })}
+            </div>
+
+            {previousPagePath && (
+                <>
+                    <Link to={previousPagePath}>Previous page</Link>
+                    <br/>
+                </>
+            )}
+            {nextPagePath && <Link to={nextPagePath}>Next page</Link>}
+        </Layout>
     )
-  }
-
-  return (
-    <Layout isHomePage>
-      <Seo title="All posts" />
-
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.title
-
-          return (
-            <li key={post.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={`/${post.slug}`} itemProp="url">
-                      <span itemProp="headline">{parse(title)}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.foreign_created_at}</small>
-                </header>
-                <section itemProp="description">{parse(post.description)}</section>
-              </article>
-            </li>
-          )
-        })}
-      </ol>
-
-      {previousPagePath && (
-        <>
-          <Link to={previousPagePath}>Previous page</Link>
-          <br />
-        </>
-      )}
-      {nextPagePath && <Link to={nextPagePath}>Next page</Link>}
-    </Layout>
-  )
 }
 
 export default BlogIndex
@@ -74,6 +87,15 @@ export const pageQuery = graphql`
         slug
         foreign_created_at(formatString: "MMMM DD, YYYY")
         title
+        featured_image {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 100
+              placeholder: TRACED_SVG
+              layout: FULL_WIDTH
+            )
+          }
+        }
       }
     }
   }
