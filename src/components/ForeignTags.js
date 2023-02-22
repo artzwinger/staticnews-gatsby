7f3a8f7f0028d4eecfeb8eda3b9bcdb616f59fc4
Art Zwinger <artzwinger@gmail.com>
2023-02-23 02:30:03 +0300

    fix tag null rendering

--- src/components/ForeignTags.js
+++ src/components/ForeignTags.js
@@ -5,9 +5,14 @@ export const ForeignTags = (post, isAmp = false) => {
     if (!post.foreign_tags) {
         return null;
     }
-    return post.foreign_tags.map((tag) => <div className="tag-link" key={`${post.id}-${tag.slug}-link`}>
-        <Link className="" to={isAmp ? `/amp/${tag.slug}/` : `/${tag.slug}/`}>
-            {tag.name}
-        </Link>
-    </div>)
+    return post.foreign_tags.map((tag) => {
+        if (!tag || !tag.slug) {
+            return null
+        }
+        return <div className="tag-link" key={`${post.id}-${tag.slug}-link`}>
+            <Link className="" to={isAmp ? `/amp/${tag.slug}/` : `/${tag.slug}/`}>
+                {tag.name}
+            </Link>
+        </div>
+    })
 };
